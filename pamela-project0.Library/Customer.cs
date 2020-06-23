667ee9b0a82f68b76019908a2322686f77eb6223
Pamela Soulis <pamela.soulis@gmail.com>
2020-06-22 22:52:53 -0400

    customer info

--- pamela-project0.Library/Customer.cs
+++ pamela-project0.Library/Customer.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace pamela_project0.Library
 {
     // customer has a first and last name
     public class Customer
     {
+        // customer has a first and last name and an ID number
+        public string customerFirstName = { get; set; }
+        public string customerLastName = { get; set; }
+        public int customerID = { get; set; } = 100
+        
 
+    
     }
 }

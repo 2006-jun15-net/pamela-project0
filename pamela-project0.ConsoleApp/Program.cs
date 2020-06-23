7051808d74579ee05bba085c6cce0bb49dc848c5
Pamela Soulis <pamela.soulis@gmail.com>
2020-06-23 00:41:58 -0400

    customer name

--- pamela-project0.ConsoleApp/Program.cs
+++ pamela-project0.ConsoleApp/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using pamela_project0.Library;
+
 
 namespace pamela_project0.ConsoleApp
 {
-    class Program
+    //class Program
+    //{
+    //    
+    //}
+    static class Program
     {
+        // customer has a first and last name and an ID number
+        //public string CustomerFirstName { get; set; }
+        //public string CustomerLastName { get; set; }
+        //public int CustomerID { get; set; } = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer numberOne = new Customer();
+
+            numberOne.CustomerFirstName = "Pamela";
+            numberOne.CustomerLastName = "Soulis";
+
+            Console.WriteLine($"The customer's name is {numberOne.CustomerFirstName} {numberOne.CustomerLastName}");
         }
+
+        //public Customer(string CustomerFirstName, string CustomerLastName)
+        //{
+
+        // this.CustomerFirstName = CustomerFirstName;
+        //this.CustomerLastName = CustomerLastName;
+        //}
+
+
     }
+
 }
--- pamela-project0.Library/Customer.cs
+++ pamela-project0.Library/Customer.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace pamela_project0.Library
 {
-    // customer has a first and last name
     public class Customer
     {
         // customer has a first and last name and an ID number
-        public string customerFirstName = { get; set; }
-        public string customerLastName = { get; set; }
-        public int customerID = { get; set; } = 100
-        
-
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        //public int CustomerID { get; set; } = 0;
+       
     
     }
 }

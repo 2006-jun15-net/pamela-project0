72f41a94284e79988d579ea2cf694f155f74d5cc
Pamela Soulis <pamela.soulis@gmail.com>
2020-06-28 20:10:03 -0400

    testing

--- pamela-project0.ConsoleApp/Program.cs
+++ pamela-project0.ConsoleApp/Program.cs
@@ -75,3 +75,26 @@ namespace pamela_project0.ConsoleApp
     }
 
 }
+
+/using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Linq;
+
+//namespace pamela_soulis_project0DataAccess
+//{
+//    public interface IMapper
+//    {
+//        //maps an EF location to a business model
+        
+//        {
+//            //
+//        }
+       
+//        {
+            
+//        }
+
+
+//    }
+//}
